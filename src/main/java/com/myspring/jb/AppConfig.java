5366a88717a9415648c5445f41aa17b531769fe8
변재호 <bjho606@naver.com>
2023-03-04 00:51:20 +0900

    [learn] AppConfig 리팩토링 - 역할, 구현 구분

--- src/main/java/com/myspring/jb/AppConfig.java
+++ src/main/java/com/myspring/jb/AppConfig.java
@@ -1,6 +1,9 @@
 package com.myspring.jb;
 
+import com.myspring.jb.discount.DiscountPolicy;
 import com.myspring.jb.discount.FixDiscountPolicy;
+import com.myspring.jb.discount.RateDiscountPolicy;
+import com.myspring.jb.member.MemberRepository;
 import com.myspring.jb.member.MemberService;
 import com.myspring.jb.member.MemberServiceImpl;
 import com.myspring.jb.member.MemoryMemberRepository;
@@ -8,11 +11,20 @@ import com.myspring.jb.order.OrderService;
 import com.myspring.jb.order.OrderServiceImpl;
 
 public class AppConfig {
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    private static DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
 }
