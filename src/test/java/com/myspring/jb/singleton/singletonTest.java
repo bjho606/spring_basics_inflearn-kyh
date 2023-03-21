5f43a4d556b90e8c3782a66315aca4bcecd41b2f
변재호 <bjho606@naver.com>
2023-03-21 20:59:29 +0900

    [learn] singleton - 스프링 컨테이너 (싱클톤 컨테이너)

--- src/test/java/com/myspring/jb/singleton/singletonTest.java
+++ src/test/java/com/myspring/jb/singleton/singletonTest.java
@@ -5,6 +5,8 @@ import com.myspring.jb.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -43,4 +45,19 @@ public class singletonTest {
         // same : ==
         // equal : .equals
     }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        // 위에 pureContainer 와 비교하기
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);    }
 }
